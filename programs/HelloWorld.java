806e0ddbd2cf2d8ee7691d541172d82fae361b45
rckukaswal <ramchandrakukaswal@gmail.com>
2025-09-25 14:45:28 +0530

    Create HelloWorld.java

---
+++ programs/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java Programming!");
+        
+        // Basic variables
+        int number = 10;
+        String message = "Java is awesome!";
+        
+        System.out.println("Number: " + number);
+        System.out.println("Message: " + message);
+    }
+}
