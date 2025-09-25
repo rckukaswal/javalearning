20b3f273cec7d59dbc4e8c779efc5181865b3715
rckukaswal <ramchandrakukaswal@gmail.com>
2025-09-25 15:17:33 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -108,56 +108,6 @@ public class Inheritance {
 
 
 
-        {id: 4,
-            title: "ifrc",
-            category: "datastructures",
-            description: "classpra",
-            filename: "ifrc.java",
-            code: `package selfpractice;
-
-public class S3 {
-
-	public static void main(String[] args) {
-
-		int science = 75;
-		int english = 25;
-		int hindi = 00;
-		float mathematics = 7.5f;
-
-		/*
-		 * int a = 75; int b = 25; int c = 00; float d = 7.5f;
-		 * 
-		 */
-
-		float a = science;
-		float b = english;
-		float c = hindi;
-		float d = mathematics;
-
-		float AVG = (a + b + c + d) / 4;
-
-		System.out.println(AVG);
-
-		if (AVG / 4 >= 90) {
-			System.out.println("Grage A");
-
-		} else if (AVG / 4 >= 75) {
-			System.out.println("Grage B");
-
-		} else if (AVG / 4 >= 50) {
-			System.out.println("Grage C");
-
-		} else if (AVG / 4 >= 35) {
-			System.out.println("Grage D");
-
-		} else {
-			System.out.println("Fail");
-		}
-
-	}
-
-}'
-},
 
 
 
@@ -171,7 +121,7 @@ public class S3 {
 
         
         {
-            id: 5,
+            id: 4,
             title: "LinkedList Implementation",
             category: "datastructures",
             description: "Basic LinkedList implementation in Java",
